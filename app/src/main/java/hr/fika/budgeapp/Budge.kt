3da6531b0324906762a3ccab79903db321274e6d
Fikaiser <borna.fikajz@gmail.com>
2023-01-06 15:52:16 +0100

    feature: Added navigation and implemented Dagger DI

---
+++ app/src/main/java/hr/fika/budgeapp/Budge.kt
@@ -0,0 +1,8 @@
+package hr.fika.budgeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Budge : Application() {
+}
--- app/src/main/java/hr/fika/budgeapp/MainActivity.kt
+++ app/src/main/java/hr/fika/budgeapp/MainActivity.kt
@@ -4,16 +4,23 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.rounded.List
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import hr.fika.budgeapp.ui.theme.BudgeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +29,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             BudgeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -29,8 +37,15 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(
-                        bottomBar = { Navigation() }
+                        bottomBar = { Navigation(navController) }
                     ) {
+                        NavHost(navController = navController, startDestination = "balance") {
+                            composable("budget") { Greeting("Budget") }
+                            composable("investment") { Greeting("Invest") }
+                            composable("balance") { Greeting("Balance") }
+                            composable("atms") { Greeting("ATMs") }
+                            composable("account") { Greeting("Account") }
+                        }
                     }
                 }
             }
@@ -43,39 +58,54 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+sealed class Screen(
+    val route: String,
+    @StringRes val resourceId: Int,
+    @DrawableRes val iconId: Int
+    ) {
+    object Budget : Screen("budget", R.string.budget, R.drawable.wallet)
+    object Investment : Screen("investment", R.string.investment, R.drawable.monitoring)
+    object Balance : Screen("balance", R.string.balance, R.drawable.balance)
+    object Atms : Screen("atms", R.string.atms, R.drawable.location_on)
+    object Account : Screen("account", R.string.account, R.drawable.account_circle)
+}
+
+val items = listOf(
+    Screen.Budget,
+    Screen.Investment,
+    Screen.Balance,
+    Screen.Atms,
+    Screen.Account
+)
+
 @Composable
-fun Navigation() {
+fun Navigation(navController: NavHostController) {
     NavigationBar {
-        NavigationBarItem(
-            icon = { Icon(painterResource(id = R.drawable.wallet), null) },
-            label = { Text("Budget") },
-            selected = false,
-            onClick = { }
-        )
-        NavigationBarItem(
-            icon = { Icon(painterResource(id = R.drawable.monitoring), null) },
-            label = { Text("Investment") },
-            selected = false,
-            onClick = { }
-        )
-        NavigationBarItem(
-            icon = { Icon(painterResource(id = R.drawable.account_balance), null) },
-            label = { Text("Balance") },
-            selected = true,
-            onClick = { }
-        )
-        NavigationBarItem(
-            icon = { Icon(painterResource(id = R.drawable.location_on), null) },
-            label = { Text("ATMs") },
-            selected = false,
-            onClick = { }
-        )
-        NavigationBarItem(
-            icon = { Icon(painterResource(id = R.drawable.account_circle), null) },
-            label = { Text("Account") },
-            selected = false,
-            onClick = { }
-        )
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        items.forEach { screen ->
+            NavigationBarItem(
+                icon = { Icon(painterResource(id = screen.iconId), contentDescription = null) },
+                label = { Text(stringResource(screen.resourceId)) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+
+        }
     }
 }
 
